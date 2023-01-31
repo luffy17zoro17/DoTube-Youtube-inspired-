040158bcc314e08da01bfb5e2e2a51001a7b3d8f
Thomas <pmthomas1991@gmail.com>
2023-02-01 00:37:35 +0530

    scroll fixed

--- components/ShortoSlide.js
+++ components/ShortoSlide.js
@@ -9,8 +9,11 @@ const ShortoSlide = () => {
         <div className="flex pt-[11rem]
            gap-[3rem] pl-[3rem] pr-[4rem]
            bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600
-           overflow-x-hidden hover:overflow-x-scroll
-           md:pl-[20rem]">
+           overflow-x-hidden 
+           overflow-x-scroll
+           md:pl-[20rem] 
+           lg:overflow-x-hidden
+           lg:hover:overflow-x-scroll">
           <h1 className="absolute text-3xl mt-[-5rem] text-yellow-500">Shorto</h1>  
           {Shorto.map((shortz)=>(
             <div key={shortz.id}>
